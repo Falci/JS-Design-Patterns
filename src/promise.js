8a8bb76436575dc4323568147bb04b3a12f1e3e6
Fernando Falci <ffalci@kenzanmedia.com>
2015-07-29 14:31:03 -0300

    promise

--- src/promise.js
+++ src/promise.js
@@ -1,6 +1,28 @@
 /**
- * Promise é usado para evitar multiplos callbacks alinhados
+ * Promise é usado para evitar multiplos callbacks alinhados.
+ * Ao invéz da função receber o callback que deve ser chamado no final da operação
+ * ela retornar uma "promessa" de que a ação será executada (ou não)
+ *
+ * Veja: 
+ *  * https://github.com/kriskowal/q
+ *  * https://docs.angularjs.org/api/ng/service/$q
+ *  * https://api.jquery.com/category/deferred-object/
  */
+
+/* Exemplo */
+function abrirFoto(caminho) {
+  var deferred = $q.defer();
+
+  funcaoAssincronaParaAbrirArquivos(caminho, function (err, arquivo){
+    if(err) {
+      deferred.reject(err);
+    } else {
+      deferred.resolve(arquivo);
+    }
+  });
+
+  return deferred.promise;
+}
 
 /* Errado */
 abrirFoto('./fotos/foto1.jpg', function (err, foto) {
